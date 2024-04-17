a354380f2f924e60b135781b83174682a21ea8f5
KHUSHBU KUMARI <131510344+Khushbu8130@users.noreply.github.com>
2024-04-17 17:40:46 +0530

    Add files via upload

---
+++ ATMMachine.java
@@ -0,0 +1,16 @@
+
+
+package ATMMachine;
+import java.util.*;
+import java.util.Scanner;
+
+public class ATMMachine {
+                public static void main(String[]args){
+                    ATM obj = new ATM();
+                    obj.checkpin();
+                }
+            }
+        
+    
+    
+
